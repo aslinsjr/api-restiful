232e017948cece08ae90b32c791f328a07d2eed0
Alexandre Lins <aslinsjr@gmail.com>
2024-11-22 14:14:12 -0300

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use((req, res, next) => {
 });
 
 app.get("/", (req, res) => {
-  res.sendFile("https://www.linkedin.com/in/aslinsjr/", { root: __dirname });
+  res.send("API Rest Mascots");
 });
 
 // Rotas Usuário
